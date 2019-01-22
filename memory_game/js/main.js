5c5b5b361ac8a80ab78db47be9004a41531f77ab
Anton <anton.mois33v@gmail.com>
2019-01-21 22:20:47 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "queen";
+var carThree = "king";
+var cardFour = "queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
